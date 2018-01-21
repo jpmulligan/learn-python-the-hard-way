d835b927198350a310b5fd5d272b2e3dab632ad7
John Mulligan <jpmulligan@gmail.com>
2018-01-20 21:23:52 -0500

    initial commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 20 21:20:15 2018
+
+@author: jpmul
+"""
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
