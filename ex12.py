92caeaf28b8fc08ba8fee0c0791927fddf6344d4
John Mulligan <jpmulligan@gmail.com>
2018-01-21 20:34:54 -0500

    ex 12 and 13

--- ex12.py
+++ ex12.py
@@ -7,3 +7,9 @@ Created on Sun Jan 21 12:59:24 2018
 
 print("LPTHW exercises complete: ", round((12.0/48.0)*100,1),"%")
 
+age = input("How old? ")
+height = input("How tall? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
---
+++ ex13.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 13:14:29 2018
+
+@author: jpmul
+"""
+
+print("LPTHW exercises complete: ", round((13.0/48.0)*100,1),"%")
+
+from sys import argv
+script, first, second, third = argv
+print("The script is called: ", script)
+print("First var:", first)
+print("Second var:", second)
+print("Third var:", third)
+
